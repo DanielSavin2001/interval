d98af22b919286e55905db0b5da310dbf6240339
Daniel Savin <daniel.savin@student.kdg.be>
2022-10-04 11:30:17 +0200

    interval opgeslagen als ondergerens en bovengrens

---
+++ Interval/src/AllTests.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntervalTest{
+	
+	class Interval{
+		//datarepresentatie
+		int ondergrens;
+		int bovengrens;	
+	}
+	
+	void setOndergrens(Interval interval, int waarde) {
+		
+	}
+	
+	
+}
